ececcc2746e9a0e65eccc09e597f3ca258fe9628
git <nitslaszlo@gmail.com>
2020-05-22 07:43:00 +0200

    up

--- MetJelentes.cs
+++ MetJelentes.cs
@@ -5,28 +5,6 @@ using System.Linq;
 
 namespace metjelentes
 {
-    class Jelentés
-    {
-        public string TelepülésKód { get; private set; }
-        public string Idő { get; private set; }
-        public string Szél { get; private set; }
-        public int Hőmérséklet { get; private set; }
-        public string IdőÚj => Idő.Insert(2, ":");
-        public string Óra => Idő.Substring(0, 2);
-        public bool Szélcsend => Szél == "00000";
-        private int Erősség => int.Parse(Szél.Substring(3, 2));
-        public string ErősségHashtag => "".PadRight(Erősség, '#');
-
-        public Jelentés(string sor)
-        {
-            string[] m = sor.Split();
-            TelepülésKód = m[0];
-            Idő = m[1];
-            Szél = m[2];
-            Hőmérséklet = int.Parse(m[3]);
-        }
-        public override string ToString() => $"{TelepülésKód} {IdőÚj} {Hőmérséklet} fok.";
-    }
     class MetJelentes
     {
         static void Main()
@@ -36,7 +14,7 @@ namespace metjelentes
 
             Console.WriteLine("2. feladat\nAdja meg egy település kódját! Település: ");
             string inputKód = Console.ReadLine();
-            Console.WriteLine($"Az utolsó mérési adat a megadott településről {jelentések.Where(x=> x.TelepülésKód == inputKód).Last().IdőÚj}-kor érkezett.");
+            Console.WriteLine($"Az utolsó mérési adat a megadott településről {jelentések.Where(x=> x.Településkód == inputKód).Last().IdőÚj}-kor érkezett.");
 
             Console.WriteLine("3. feladat");
             Console.WriteLine($"A legalacsonyabb hőmérséklet: {jelentések.OrderBy(x=>x.Hőmérséklet).First()}");
@@ -44,14 +22,14 @@ namespace metjelentes
 
             Console.WriteLine("4. feladat");
             var szélcsend = jelentések.Where(x => x.Szélcsend);
-            if (szélcsend.Count() != 0) Console.Write(szélcsend.Aggregate("", (c, n) => c += $"{n.TelepülésKód} {n.IdőÚj}\n"));
+            if (szélcsend.Count() != 0) Console.Write(szélcsend.Aggregate("", (c, n) => c += $"{n.Településkód} {n.IdőÚj}\n"));
             else Console.WriteLine("Nem volt szélcsend a mérések idején.");
 
             Console.WriteLine("5. feladat");
-            HashSet<string> telpülésKódok = new HashSet<string>(jelentések.Select(x=>x.TelepülésKód));
+            HashSet<string> telpülésKódok = new HashSet<string>(jelentések.Select(x=>x.Településkód));
             foreach (var i in telpülésKódok)
             {
-                var aktTelepülésMérései = jelentések.Where(x => x.TelepülésKód == i);
+                var aktTelepülésMérései = jelentések.Where(x => x.Településkód == i);
                 int min = aktTelepülésMérései.Min(x => x.Hőmérséklet);
                 int max = aktTelepülésMérései.Max(x => x.Hőmérséklet);
                 int ingadozás = max - min;
@@ -65,9 +43,9 @@ namespace metjelentes
             foreach (var i in telpülésKódok)
             {
                 List<string> ki = new List<string>() {i};
-                foreach (var j in jelentések.Where(x => x.TelepülésKód == i))
+                foreach (var j in jelentések.Where(x => x.Településkód == i))
                 {
-                    ki.Add($"{j.IdőÚj} {j.ErősségHashtag}");
+                    ki.Add($"{j.IdőÚj} {j.SzélerősségHashtag}");
                 }
                 File.WriteAllLines($"{i}.txt", ki);
             }
